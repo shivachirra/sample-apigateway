7fd53ed71cc819d39c75de384639d8e9758922df
siva <nagireddy.chirra@gmail.com>
2022-07-05 11:25:15 +0530

    Created apigateway

---
+++ modules/lambda_function/function_code/index.js
@@ -0,0 +1,17 @@
+const test = [
+	"Schindlers List",
+	"Shawshank Redemption",
+	"Batman The Dark Knight",
+	"Spider-Man : No Way Home",
+	"Avengers",
+];
+
+exports.handler = async (event) => {
+	return {
+		statusCode: 200,
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ test }), //sending the array of test as stringified JSON in the response
+	};
+};
